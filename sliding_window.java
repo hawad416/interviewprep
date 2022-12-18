987b06f555347e0fa13ae1c42e3c8454fb790094
Hawa Ali <hawad416@gmail.com>
2022-12-18 00:59:06 -0800

    k

--- sliding_window.java
+++ sliding_window.java
@@ -61,37 +61,4 @@ public class sliding_window {
 
         return max; 
     }
-
-
-    // valid  paranthesis: https://leetcode.com/problems/valid-parentheses/description/
-    public static boolean isValid(String s){
-        if(s == null) throw new IllegalArgumentException("string in invalid");
-
-        Map<Character, Character> paranMap = new HashMap<>();
-        Stack<Character> paranStack = new Stack<>();
-
-        paranMap.put('(', ')');
-        paranMap.put('{', '}');
-        paranMap.put('[', ']');
-
-
-        for(int i = 0; i < s.length(); i++){
-
-            if(paranMap.containsKey(s.charAt(i))){
-                paranStack.push(s.charAt(i));
-            } else{     
-                
-                if(paranStack.isEmpty()) return false; 
-
-                char mostRecentParan = paranStack.pop();
-                if(paranStack.get(mostRecentParan) != s.charAt(i)) return false;
-            }
-        }
-
-        if(!paranStack.isEmpty()) return false;
-
-        return true; 
-    }
-
-
 }
