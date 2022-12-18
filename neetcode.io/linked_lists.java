9f84b497fad5146db5ffd6482d5122928ae54e48
Hawa Ali <hawad416@gmail.com>
2022-12-18 15:43:33 -0800

    merge 2 sorted list: https://leetcode.com/problems/merge-two-sorted-lists/description/

--- neetcode.io/linked_lists.java
+++ neetcode.io/linked_lists.java
@@ -60,4 +60,43 @@ public class linked_lists {
 
         return head;
     }
+
+
+
+
+
+    // merge two sorted linked list : https://leetcode.com/problems/merge-two-sorted-lists/description/
+
+    // extra memory 
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+        ListNode list3 = new ListNode(1);
+        ListNode curr = list3; 
+
+        while(list1 != null && list2 != null){
+            if(list1.val <= list2.val){
+                curr.next = new ListNode(list1.val);
+                curr = curr.next; 
+                list1 = list1.next; 
+            } else{
+                curr.next = new ListNode(list2.val);
+                curr = curr.next; 
+                list2 = list2.next; 
+            }
+        }
+
+        while(list1 != null){
+            curr.next = new ListNode(list1.val);
+            curr = curr.next;
+            list1 = list1.next;
+        }
+
+         while(list2 != null){
+            curr.next = new ListNode(list2.val);
+            curr = curr.next;
+            list2 = list2.next;
+        }
+
+        return list3.next;
+    }
 }
